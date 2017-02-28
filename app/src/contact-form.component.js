96be97dcdacf6c2fe444be68a728c88008bf091a
NicholasPiano <nicholas.d.piano@gmail.com>
2017-02-28 22:30:22 +0000

    done.

--- app/src/contact-form.component.js
+++ app/src/contact-form.component.js
@@ -1,15 +1,21 @@
 export default {
 	template:
 `
-<form>
+<form name="contactForm">
 	<label for="fname">First name</label>
-	<input type="text" name="fname" id="fname" ng-model="$ctrl.model.fname"><br>
-	<button type="submit" ng-click="onClickedSubmit()">Add</button>
-	<button type="submit" ng-click="onClickedRemoveAll()">Remove all</button>
+	<input type="text" name="fname" id="fname" ng-model="$ctrl.model.fname" required><br>
+	<span style="color:red" ng-show="nameError()">
+		<span ng-show="contactForm.fname.$error.required">First name is required.</span>
+	</span><br>
+
+	<button type="submit" ng-click="onClickedSubmit()" ng-disabled="cantSubmit()">Add</button>
+	<button ng-click="onClickedRemoveAll()">Remove all</button>
 </form>
 `,
 	controller: ($scope, $window, ContactsService) => {
 		$scope.ngModel = $scope.ngModel || {};
+
+		// actions
 		$scope.onClickedSubmit = () => {
 			let model = angular.copy($scope.$ctrl.model);
 			$scope.$ctrl.model = {};
@@ -18,6 +24,18 @@ export default {
 		$scope.onClickedRemoveAll = () => {
 			$scope.$ctrl.model = {};
 			ContactsService.removeAll();
+		}
+
+		// tests
+		$scope.cantSubmit = () => {
+			if ($scope.contactForm) {
+				return $scope.contactForm.fname.$dirty && $scope.contactForm.fname.$invalid || $scope.contactForm.fname.$pristine;
+			} else {
+				return true;
+			}
+		};
+		$scope.nameError = () => {
+			return $scope.contactForm.fname.$dirty && $scope.contactForm.fname.$invalid && $scope.contactForm.fname.$modelValue;
 		}
 	},
 	bindings: {
